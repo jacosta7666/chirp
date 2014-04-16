cc75e5ae5d13270d7fc5148944882d2b02932874
George Acosta <jacosta7666@gmail.com>
2014-04-15 20:27:50 -0700

    Added configuration

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 /**
  * Created by Air on 4/15/14.
  */
+var express = require('express');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+//sets where the programs state is at ie development (more control_ ie production (less)
+
+var app = express();
+//start express
+var config = require('./lib/config');
+
+
+
+app.listen(config.port, function() {
+    console.log('running on port ' + config.port);
+
+});
+
+//expose the app to export
+exports = module.exports = app;
+
